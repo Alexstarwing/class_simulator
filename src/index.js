b24958f8a0a055b639f10e3877a67ff751f90835
Alexstarwing <441698225@qq.com>
2023-01-20 13:27:45 -0500

    dropdown draft

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from 'react-dom';
+import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return(
+        <div>
+            <h1>Hello World!</h1>
+            <label><h2>Select a class</h2></label><br></br>
+                <div className={"dropdown"}>
+                    <button className={"dropbtn"}>Dropdown</button>
+                    <div className={"dropdown-content"}>
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </div>
+                </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
